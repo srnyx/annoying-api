24297970284cbd3fc0571040e19747bcf721e1ed
srnyx <25808801+srnyx@users.noreply.github.com>
2022-12-10 15:38:22 -0500

    Removed `AnnoyingMessage#send(TextChannel)`

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,21 +6,17 @@ repositories {
     mavenCentral() // org.spigotmc:spigot, net.md-5:bungeecord-api
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/") // org.spigotmc:spigot
     maven("https://oss.sonatype.org/content/repositories/snapshots/") // org.spigotmc:spigot
-    maven("https://m2.dv8tion.net/releases") // com.discordsrv:discordsrv
-    maven("https://nexus.scarsz.me/content/groups/public") // com.discordsrv:discordsrv
 }
 
 dependencies {
     compileOnly("org.spigotmc", "spigot-api", "1.11-R0.1-SNAPSHOT")
     compileOnly("net.md-5", "bungeecord-api", "1.16-R0.4")
-    compileOnlyApi("com.discordsrv", "discordsrv", "1.26.0")
     compileOnly("org.jetbrains", "annotations", "23.0.0")
     compileOnly(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
 }
 
 plugins {
     java
-    `java-library`
     `maven-publish`
 }
 
