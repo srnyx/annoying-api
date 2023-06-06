98cd7a3d922bed0bb74e79403fe998eaa30abe92
srnyx <25808801+srnyx@users.noreply.github.com>
2023-06-06 15:26:28 -0400

    Revert "Fix `org.jetbrains:annotations` dependency"
    This reverts commit 1c41bf925bbeb1bcd49106be0e929889a55d1726.

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 dependencies {
-    compileOnly("org.jetbrains", "annotations", "24.0.0")
+    compileOnlyApi("org.jetbrains", "annotations", "24.0.0")
     api("org.bstats", "bstats-bukkit", "3.0.0")
     api("de.tr7zw", "item-nbt-api", "2.11.2")
 }
