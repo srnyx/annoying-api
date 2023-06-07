403d5ef15633fc1e9e4382804408a05381d4b029
srnyx <25808801+srnyx@users.noreply.github.com>
2023-06-06 20:47:48 -0400

    Exclude ItemNBT-API annotations

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -6,7 +6,9 @@ plugins {
 dependencies {
     compileOnlyApi("org.jetbrains", "annotations", "24.0.0")
     api("org.bstats", "bstats-bukkit", "3.0.0")
-    api("de.tr7zw", "item-nbt-api", "2.11.2")
+    api("de.tr7zw", "item-nbt-api", "2.11.2") {
+        exclude("de.tr7zw", "functional-annotations")
+    }
 }
 
 // Javadoc JAR task
@@ -29,7 +31,6 @@ configure<PublishingExtension> {
 tasks {
     shadowJar {
         relocate("org.bstats", "xyz.srnyx.annoyingapi.bstats")
-        relocate("de.tr7zw.annotations", "xyz.srnyx.annoyingapi.nbtapi.annotations")
         relocate("de.tr7zw.changeme.nbtapi", "xyz.srnyx.annoyingapi.nbtapi")
     }
 }
