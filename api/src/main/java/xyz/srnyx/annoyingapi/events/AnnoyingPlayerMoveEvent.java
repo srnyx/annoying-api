ae72c1d91213ad7abf8b0710fdf0dc4063769f78
srnyx <25808801+srnyx@users.noreply.github.com>
2023-06-23 18:11:21 -0400

    Calculate movement type on get

--- api/src/main/java/xyz/srnyx/annoyingapi/events/AnnoyingPlayerMoveEvent.java
+++ api/src/main/java/xyz/srnyx/annoyingapi/events/AnnoyingPlayerMoveEvent.java
@@ -6,6 +6,7 @@ import org.bukkit.event.HandlerList;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 
 /**
@@ -20,7 +21,7 @@ public class AnnoyingPlayerMoveEvent extends PlayerMoveEvent {
     /**
      * The {@link MovementType type of movement} that the player has done
      */
-    @NotNull private final MovementType movementType;
+    @Nullable private MovementType movementType;
 
     /**
      * Constructs a new {@link AnnoyingPlayerMoveEvent}
@@ -31,17 +32,6 @@ public class AnnoyingPlayerMoveEvent extends PlayerMoveEvent {
      */
     public AnnoyingPlayerMoveEvent(@NotNull Player player, @NotNull Location from, @NotNull Location to) {
         super(player, from, to);
-
-        // movementType
-        if (from.getYaw() == to.getYaw() && from.getPitch() == to.getPitch()) {
-            movementType = MovementType.TRANSLATION;
-            return;
-        }
-        if (from.getX() == to.getX() && from.getY() == to.getY() && from.getZ() == to.getZ()) {
-            movementType = MovementType.ROTATION;
-            return;
-        }
-        movementType = MovementType.BOTH;
     }
 
     /**
@@ -80,6 +70,16 @@ public class AnnoyingPlayerMoveEvent extends PlayerMoveEvent {
      */
     @NotNull
     public MovementType getMovementType() {
+        if (movementType != null) return movementType;
+        final Location from = getFrom();
+        final Location to = getTo();
+        if (from.getYaw() == to.getYaw() && from.getPitch() == to.getPitch()) {
+            movementType = MovementType.TRANSLATION;
+        } else if (from.getX() == to.getX() && from.getY() == to.getY() && from.getZ() == to.getZ()) {
+            movementType = MovementType.ROTATION;
+        } else {
+            movementType = MovementType.BOTH;
+        }
         return movementType;
     }
 
