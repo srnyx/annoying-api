c14e981fe748d06ec26916d62aac0efdb07d9d32
srnyx <25808801+srnyx@users.noreply.github.com>
2023-06-26 00:20:09 -0400

    Bump version (3.1.0)

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ import xyz.srnyx.gradlegalaxy.utility.spigotAPI
 
 plugins {
     java
-    id("xyz.srnyx.gradle-galaxy") version "1.0.1" apply false
+    id("xyz.srnyx.gradle-galaxy") version "1.0.2" apply false
     id("com.github.johnrengelman.shadow") version "8.1.1" apply false
 }
 
@@ -15,7 +15,7 @@ subprojects {
     apply(plugin = "xyz.srnyx.gradle-galaxy")
     apply(plugin = "com.github.johnrengelman.shadow")
 
-    setupMC("xyz.srnyx", "3.0.1")
+    setupMC("xyz.srnyx", "3.1.0")
     spigotAPI("1.8.8")
     repository(Repository.PLACEHOLDER_API)
     dependencies.compileOnly("me.clip", "placeholderapi", "2.11.3")
