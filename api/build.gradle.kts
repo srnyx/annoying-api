8ad60129cef52f1152b24c0a4274fbf83d845877
srnyx <25808801+srnyx@users.noreply.github.com>
2023-07-31 19:54:35 -0400

    Exclude SLF4J

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -19,6 +19,7 @@ dependencies {
     compileOnlyApi("org.jetbrains", "annotations", "24.0.0")
     implementationRelocate(project, "org.reflections:reflections:0.10.2") {
         exclude("com.google.code.findbugs", "jsr305")
+        exclude("org.slf4j", "slf4j-api")
     }
     implementationRelocate(project, "org.bstats:bstats-bukkit:3.0.0")
     implementationRelocate(project, "de.tr7zw:item-nbt-api:2.11.3", "de.tr7zw.changeme.nbtapi").exclude("de.tr7zw", "functional-annotations")
