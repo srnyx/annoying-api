a6b6af8aff96248861cdcab0339602e08ce2ed1a
srnyx <25808801+srnyx@users.noreply.github.com>
2023-09-22 17:51:52 -0400

    Update Item NBT API

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -23,7 +23,7 @@ dependencies {
         exclude("org.slf4j", "slf4j-api")
     }
     implementationRelocate(project, "org.bstats:bstats-bukkit:3.0.2")
-    implementationRelocate(project, "de.tr7zw:item-nbt-api:2.12.0-RC1", "de.tr7zw.changeme.nbtapi").exclude("de.tr7zw", "functional-annotations")
+    implementationRelocate(project, "de.tr7zw:item-nbt-api:2.12.0", "de.tr7zw.changeme.nbtapi").exclude("de.tr7zw", "functional-annotations")
 }
 
 setupPublishing(
