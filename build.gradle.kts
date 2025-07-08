fb517daba5edf2be1395a6858958b3b4c6163aa6
srnyx <25808801+srnyx@users.noreply.github.com>
2025-07-08 18:49:50 -0400

    Update dependencies
    Gradle: 8.14.2 -> 8.14.3
    Shadow: 8.3.7 -> 8.3.8
    Item NBT API: 2.15.0 -> 2.15.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     java
     `java-library`
     id("xyz.srnyx.gradle-galaxy") version "1.3.3"
-    id("com.gradleup.shadow") version "8.3.7"
+    id("com.gradleup.shadow") version "8.3.8"
     id("net.kyori.blossom") version "2.1.0"
     id("org.jetbrains.gradle.plugin.idea-ext") version "1.1.10"
 }
@@ -24,7 +24,7 @@ sourceSets.main { blossom.javaSources { property("annoying_api_version", version
 repository(Repository.JITPACK, Repository.PLACEHOLDER_API, Repository.ALESSIO_DP, Repository.CODE_MC)
 dependencies {
     compileOnly("me.clip", "placeholderapi", "2.11.6")
-    compileOnly("de.tr7zw", "item-nbt-api", "2.15.0") // Downloaded on runtime
+    compileOnly("de.tr7zw", "item-nbt-api", "2.15.1") // Downloaded on runtime
     compileOnlyApi("org.bstats", "bstats-bukkit", "3.1.0") // Downloaded on runtime
     compileOnlyApi("org.reflections", "reflections", "0.10.2") // Downloaded on runtime
     compileOnlyApi("com.h2database", "h2", "2.2.224") // Downloaded on runtime (don't update to keep support for Java 8)
