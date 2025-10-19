a989de2db6903b12dcbbb317433610544050dbb6
srnyx <25808801+srnyx@users.noreply.github.com>
2025-10-19 18:48:18 -0400

    Update dependencies
    Gradle: 9.0.0 -> 9.1.0
    Gradle Galaxy: 1.3.5 -> 2.0.2
    Shadow: 8.3.8 -> 8.3.9
    Blossom: 2.1.0 -> 2.2.0
    Item NBT API: 2.15.2 -> 2.15.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import xyz.srnyx.gradlegalaxy.data.config.DependencyConfig
+import xyz.srnyx.gradlegalaxy.data.config.JavaSetupConfig
 import xyz.srnyx.gradlegalaxy.data.pom.DeveloperData
 import xyz.srnyx.gradlegalaxy.data.pom.LicenseData
 import xyz.srnyx.gradlegalaxy.enums.Repository
@@ -8,14 +10,17 @@ import xyz.srnyx.gradlegalaxy.utility.*
 plugins {
     java
     `java-library`
-    id("xyz.srnyx.gradle-galaxy") version "1.3.5"
-    id("com.gradleup.shadow") version "8.3.8"
-    id("net.kyori.blossom") version "2.1.0"
+    id("xyz.srnyx.gradle-galaxy") version "2.0.2"
+    id("com.gradleup.shadow") version "8.3.9"
+    id("net.kyori.blossom") version "2.2.0"
     id("org.jetbrains.gradle.plugin.idea-ext") version "1.3"
 }
 
-spigotAPI("1.8.8")
-setupMC("xyz.srnyx", "5.2.0", "General purpose API with tons of features")
+spigotAPI(config = DependencyConfig(version = "1.8.8"))
+setupMC(javaSetupConfig = JavaSetupConfig(
+    group = "xyz.srnyx",
+    version = "5.2.0",
+    description = "General purpose API with tons of features"))
 
 // Blossom (see java-templates module)
 sourceSets.main { blossom.javaSources { property("annoying_api_version", version.toString()) } }
@@ -24,14 +29,14 @@ sourceSets.main { blossom.javaSources { property("annoying_api_version", version
 repository(Repository.JITPACK, Repository.PLACEHOLDER_API, Repository.ALESSIO_DP, Repository.CODE_MC)
 dependencies {
     compileOnly("me.clip", "placeholderapi", "2.11.6")
-    compileOnly("de.tr7zw", "item-nbt-api", "2.15.2") // Downloaded on runtime
+    compileOnly("de.tr7zw", "item-nbt-api", "2.15.3") // Downloaded on runtime
     compileOnlyApi("org.bstats", "bstats-bukkit", "3.1.0") // Downloaded on runtime
     compileOnlyApi("org.reflections", "reflections", "0.10.2") // Downloaded on runtime
     compileOnlyApi("com.h2database", "h2", "2.2.224") // Downloaded on runtime (don't update to keep support for Java 8)
     compileOnlyApi("org.postgresql", "postgresql", "42.7.7") // Downloaded on runtime
     compileOnlyApi("org.jetbrains", "annotations", "26.0.2")
-    implementationRelocate(project, "net.byteflux:libby-bukkit:1.3.1", "net.byteflux.libby")
-    implementationRelocate(project, "xyz.srnyx:java-utilities:5b11020f87", "xyz.srnyx.javautilities")
+    implementationRelocate("net.byteflux:libby-bukkit:1.3.1", "net.byteflux.libby")
+    implementationRelocate("xyz.srnyx:java-utilities:5b11020f87", "xyz.srnyx.javautilities")
 }
 
 // Publishing
