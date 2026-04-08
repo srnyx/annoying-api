6e9bc5a819bb37ac96f03f1a676e8a26778fb7bd
srnyx <25808801+srnyx@users.noreply.github.com>
2026-04-07 21:00:59 -0400

    Use new publishing system

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,20 @@
 import xyz.srnyx.gradlegalaxy.data.config.DependencyConfig
 import xyz.srnyx.gradlegalaxy.data.config.JavaSetupConfig
+import xyz.srnyx.gradlegalaxy.data.config.publishingSimpleConfig
 import xyz.srnyx.gradlegalaxy.data.pom.DeveloperData
 import xyz.srnyx.gradlegalaxy.data.pom.LicenseData
 import xyz.srnyx.gradlegalaxy.enums.Repository
 import xyz.srnyx.gradlegalaxy.enums.repository
-import xyz.srnyx.gradlegalaxy.utility.*
+import xyz.srnyx.gradlegalaxy.utility.implementationRelocate
+import xyz.srnyx.gradlegalaxy.utility.setupMC
+import xyz.srnyx.gradlegalaxy.utility.setupPublishingEnv
+import xyz.srnyx.gradlegalaxy.utility.spigotAPI
 
 
 plugins {
     java
     `java-library`
-    id("xyz.srnyx.gradle-galaxy") version "2.0.2"
+    id("xyz.srnyx.gradle-galaxy") version "2.1.0"
     id("com.gradleup.shadow") version "8.3.9"
     id("net.kyori.blossom") version "2.2.0"
     id("org.jetbrains.gradle.plugin.idea-ext") version "1.3"
@@ -40,22 +44,22 @@ sourceSets.main { blossom.javaSources {
 } }
 
 // Dependencies
-repository(Repository.JITPACK, Repository.PLACEHOLDER_API, Repository.ALESSIO_DP, Repository.CODE_MC)
+repository(Repository.SRNYX_RELEASES, Repository.SRNYX_SNAPSHOTS, Repository.PLACEHOLDER_API, Repository.ALESSIO_DP, Repository.CODE_MC)
 dependencies {
-    compileOnly("me.clip", "placeholderapi", "2.11.6")
-    compileOnly("de.tr7zw", "item-nbt-api", itemNbtApiVersion) // Downloaded on runtime
-    compileOnlyApi("org.bstats", "bstats-bukkit", bStatsVersion) // Downloaded on runtime
-    compileOnlyApi("org.reflections", "reflections", reflectionsVersion) // Downloaded on runtime
-    compileOnlyApi("com.h2database", "h2", h2Version) // Downloaded on runtime (don't update to keep support for Java 8)
-    compileOnlyApi("org.postgresql", "postgresql", postgreSqlVersion) // Downloaded on runtime
-    compileOnlyApi("org.jetbrains", "annotations", "26.0.2")
+    compileOnly("me.clip:placeholderapi:2.11.6")
+    compileOnly("de.tr7zw:item-nbt-api:$itemNbtApiVersion") // Downloaded on runtime
+    compileOnlyApi("org.bstats:bstats-bukkit:$bStatsVersion") // Downloaded on runtime
+    compileOnlyApi("org.reflections:reflections:$reflectionsVersion") // Downloaded on runtime
+    compileOnlyApi("com.h2database:h2:$h2Version") // Downloaded on runtime (don't update to keep support for Java 8)
+    compileOnlyApi("org.postgresql:postgresql:$postgreSqlVersion") // Downloaded on runtime
+    compileOnlyApi("org.jetbrains:annotations:26.0.2")
     implementationRelocate("net.byteflux:libby-bukkit:1.3.1", "net.byteflux.libby")
-    implementationRelocate("xyz.srnyx:java-utilities:5b11020f87", "xyz.srnyx.javautilities")
+    implementationRelocate("xyz.srnyx:java-utilities:4a0f974", "xyz.srnyx.javautilities")
 }
 
 // Publishing
-setupPublishing(
+setupPublishingEnv(publishingSimpleConfig(
     artifactId = "annoying-api",
     url = "https://annoying-api.srnyx.com",
     licenses = listOf(LicenseData.MIT),
-    developers = listOf(DeveloperData.srnyx))
+    developers = listOf(DeveloperData.srnyx)))
