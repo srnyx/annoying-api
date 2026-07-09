309e654b9c5a8959647268d94079ff2951b8d22a
srnyx <25808801+srnyx@users.noreply.github.com>
2026-07-09 00:49:49 -0400

    Fix config onLoad and add `RootConfig#reload()`

--- src/main/java/xyz/srnyx/annoyingapi/file/okaeri/RootConfig.java
+++ src/main/java/xyz/srnyx/annoyingapi/file/okaeri/RootConfig.java
@@ -1,18 +1,13 @@
 package xyz.srnyx.annoyingapi.file.okaeri;
 
 import eu.okaeri.configs.OkaeriConfig;
-import org.jetbrains.annotations.NotNull;
-
-import java.io.InputStream;
 
 
 public class RootConfig extends OkaeriConfig {
     public void onLoad() {}
 
-    @Override @NotNull
-    public OkaeriConfig load(@NotNull InputStream stream) {
-        final OkaeriConfig config = super.load(stream);
+    public void reload() {
+        this.load(true);
         this.onLoad();
-        return config;
     }
 }
