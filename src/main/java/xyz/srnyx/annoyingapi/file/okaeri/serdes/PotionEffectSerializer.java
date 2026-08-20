dd4052c127d02796823e2f1296c90965d209c23a
srnyx <25808801+srnyx@users.noreply.github.com>
2026-08-19 23:21:40 -0400

    Make amplifier, ambient, particles optional for PotionEffectSerializer

--- src/main/java/xyz/srnyx/annoyingapi/file/okaeri/serdes/PotionEffectSerializer.java
+++ src/main/java/xyz/srnyx/annoyingapi/file/okaeri/serdes/PotionEffectSerializer.java
@@ -9,9 +9,7 @@ import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import static xyz.srnyx.annoyingapi.reflection.org.bukkit.potion.RefPotionEffect.POTION_EFFECT_CONSTRUCTOR_1_13;
 import static xyz.srnyx.annoyingapi.reflection.org.bukkit.potion.RefPotionEffect.POTION_EFFECT_HAS_ICON_METHOD;
@@ -51,17 +49,14 @@ public class PotionEffectSerializer implements ObjectSerializer<PotionEffect> {
         final PotionEffectType type = PotionEffectType.getByName(typeName);
         if (type == null) throw new IllegalArgumentException(EnumMatcher.suggest(typeName, POTION_EFFECT_TYPE_NAMES, 5));
 
-        // duration, amplifier, ambient, particles
-        final List<String> missing = new ArrayList<>();
+        // duration
         final Integer duration = data.get("duration", Integer.class);
-        if (duration == null) missing.add("duration");
-        final Integer amplifier = data.get("amplifier", Integer.class);
-        if (amplifier == null) missing.add("amplifier");
-        final Boolean ambient = data.get("ambient", Boolean.class);
-        if (ambient == null) missing.add("ambient");
-        final Boolean particles = data.get("particles", Boolean.class);
-        if (particles == null) missing.add("particles");
-        if (!missing.isEmpty()) throw new IllegalArgumentException("Missing required field(s): "+ String.join(", ", missing));
+        if (duration == null) throw new IllegalArgumentException("Missing required field: duration");
+
+        // amplifier, ambient, particles
+        final Integer amplifier = data.getOr("amplifier", Integer.class, 0);
+        final Boolean ambient = data.getOr("ambient", Boolean.class, false);
+        final Boolean particles = data.getOr("particles", Boolean.class, true);
 
         // 1.13+ icon
         final Boolean icon = data.get("icon", Boolean.class);
